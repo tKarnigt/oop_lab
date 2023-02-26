392dc17eac87a4b6a25ac69593fc1d982fab4e28
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 20:04:20 +0700

    Create App.java

---
+++ lab_6/lab6_2/App.java
@@ -0,0 +1,13 @@
+public class App
+{
+    public static void main(String[] args) {
+        Course course = new Course("Math");
+        course.addStudent("a");
+        course.addStudent("b");
+        course.addStudent("c");
+        course.dropStudent("b");
+        for (int i = 0; i < course.getNumberOfStudents(); i++) {
+            System.out.println("Student " + (i + 1) + " is " + course.getStudents()[i]);
+        }
+    }
+}
