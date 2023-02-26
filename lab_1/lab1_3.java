b78227756ef1fd064721c02258d3dc7dedc07bec
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 19:37:27 +0700

    Create lab1_3.java

---
+++ lab_1/lab1_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter a number between 0 to 1000: ");
+        int num = scn.nextInt();
+        if(num >= 0 && num <= 1000)
+        {
+            int ans = num/1000+((num/100)%10)+((num/10)%10)+(num%10);
+            System.out.print(""+ans);
+        }
+        else System.out.print("This number is out of range!");
+    }
+}
