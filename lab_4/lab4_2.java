5c827c7d0a8cbaa6aa7956bd17fad88f27344250
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 19:51:51 +0700

    Create lab4_2.java

---
+++ lab_4/lab4_2.java
@@ -0,0 +1,18 @@
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+public class lab4_2
+{
+    public static void main(String[] args)
+    {
+        GregorianCalendar g = new GregorianCalendar();
+        System.out.println("Current year, month, date, and day of week");
+        System.out.println("Year is "+g.get(GregorianCalendar.YEAR));
+        System.out.println("Month is "+g.get(GregorianCalendar.MONTH));
+        System.out.println("Date is "+g.get(GregorianCalendar.DATE));
+        System.out.println("Day of week is "+g.get(GregorianCalendar.DAY_OF_WEEK));
+        System.out.println("------------");
+        g.add(Calendar.DATE, 1);
+        System.out.print("After specified the elapsed time of one day after current day \n" + "Year is "+g.get(GregorianCalendar.YEAR) + "\nMonth is "+g.get(GregorianCalendar.MONTH) + "\nDate is "+g.get(GregorianCalendar.DATE) + "\nDay of week is"+g.get(GregorianCalendar.DAY_OF_WEEK)+"\n");
+        System.out.println(g.getTime());
+    }
+}
