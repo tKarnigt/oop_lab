77efc8bcd511f94ef8c3c9f785a2bdea5d741e23
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 19:58:54 +0700

    Update App.java

--- lab_5/lab5_1/App.java
+++ lab_5/lab5_1/App.java
@@ -1,5 +1,5 @@
 package com.company;
-public class Lab5_1_64010022
+public class App
 {
     public static void main(String[] args) throws Exception {
         com.company.Account account = new com.company.Account(1122, 20000, 4.5);
