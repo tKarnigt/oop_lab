e7fe03f0dee669df528b2d29343ef88e1bcbb283
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 19:35:55 +0700

    Create lab1_1.java

---
+++ lab_1/lab1_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        int pplt = 312032486;
+        int birth = 0;
+        int death = 0;
+        int immi = 0;
+        for (int i = 1 ; i <= 5 ; i++)
+        {
+            birth = (365 * i * 24 * 60 * 60) / 7;
+            death = (365 * i * 24 * 60 * 60) / 13;
+            immi = (365 * i * 24 * 60 * 60) / 45;
+            System.out.println("Population in year " + i + " is " + (pplt +
+                    birth - death + immi));
+        }
+    }
+}
