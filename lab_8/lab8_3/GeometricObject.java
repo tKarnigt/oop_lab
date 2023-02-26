2982ff58c96d909ec2bf15c0b5c0c8abbcde5478
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 20:26:07 +0700

    Create GeometricObject.java

---
+++ lab_8/lab8_3/GeometricObject.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+abstract class GeometricObject{
+	private String color;
+	private boolean filled;
+	private Date dateCreated;
+
+	protected GeometricObject(){
+		this.color = "Black";
+		this.filled = false;
+		this.dateCreated = new Date();
+	}
+
+	protected GeometricObject(String color,boolean filled){
+		this.color = color;
+		this.filled = filled;
+		this.dateCreated = new Date();
+	}
+
+	public void setColor(String color){
+		this.color = color;
+	}
+
+	public void setFilled(){
+		this.filled = filled;
+	}
+
+	public String getColor(){
+		return color;
+	}
+
+	public boolean isFilled(){
+		return filled;
+	}
+
+	public Date getDate(){
+		return dateCreated;
+	}
+
+	abstract public double getArea();
+	abstract public double getPerimeter();
+
+	@Override
+	public String toString(){
+		return "Date Created: "+dateCreated+"\nColor: "+color+"\nFilled: "+filled;
+	}
+}
